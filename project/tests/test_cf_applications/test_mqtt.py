f9e583ac91d32b6e957823f251edb2e2df334049
mraczyns <marta.raczynska@intel.com>
2015-08-18 11:40:32 +0200

    Adapting test mqtt to changing in application

--- project/tests/test_cf_applications/test_mqtt.py
+++ project/tests/test_cf_applications/test_mqtt.py
@@ -1,11 +1,13 @@
 import os
+import signal
 import ssl
+import subprocess
+import time
 import unittest
 
 import paho.mqtt.client as mqtt
 
-from test_utils import app_source_utils
-from test_utils import ApiTestCase, get_logger, TEST_SETTINGS
+from test_utils import ApiTestCase, get_logger, config, app_source_utils, TEST_SETTINGS
 from test_utils.cli import cloud_foundry as cf
 from test_utils.objects import Application, Organization, Space
 
@@ -21,9 +23,9 @@ class TestMqtt(ApiTestCase):
     MQTT_SERVICE_NAME = "mosquitto14"
     MQTT_SERVICE_INSTANCE_NAME = "mqtt-demo-messages"
     APP_REPO_PATH = "../../mqtt-demo"
-    TEST_DATA_FILE = os.path.join(APP_REPO_PATH, "tools", "shuttle_scale_cut_val.csv")
+    TEST_DATA_FILE = os.path.join(os.path.dirname(__file__), "shuttle_scale_cut_val.csv")
     APP_NAME = "mqtt-demo"
-    MQTT_SERVER = "mqtt-demo.demo-gotapaas.com"
+    MQTT_SERVER = APP_NAME + '.' + config.TEST_SETTINGS["TEST_ENVIRONMENT"]
     SERVER_CERTIFICATE = os.path.join(os.path.dirname(__file__), "mosquitto_demo_cert.pem")
     MQTT_TOPIC_NAME = "space-shuttle/test-data"
 
@@ -43,6 +45,9 @@ class TestMqtt(ApiTestCase):
         credentials = application.cf_env()["VCAP_SERVICES"][self.MQTT_SERVICE_NAME][0]["credentials"]
         port_mosquitto = credentials["port"]
 
+        logs = subprocess.Popen(["cf","logs","mqtt-demo"], stdout=subprocess.PIPE)
+        time.sleep(5)
+
         mqtt_client = mqtt.Client()
         mqtt_client.username_pw_set(credentials["username"], credentials["password"])
         mqtt_client.tls_set(self.SERVER_CERTIFICATE, tls_version=ssl.PROTOCOL_TLSv1_2)
@@ -57,6 +62,22 @@ class TestMqtt(ApiTestCase):
         connection_code = mqtt_client.disconnect()
         logger.info("Disconnected with code {}".format(connection_code))
 
+        grep =  subprocess.Popen(["grep","message:"], stdin = logs.stdout, stdout=subprocess.PIPE)
+        logs.stdout.close()
+        time.sleep(50)
+        os.kill(logs.pid, signal.SIGTERM)
+        cut = subprocess.Popen("cut -d ':' -f7 ", stdin = grep.stdout, stdout=subprocess.PIPE, shell=True)
+        grep.stdout.close()
+        output = cut.communicate()[0]
+
+        log_result = str(output)
+        log_result = log_result.replace("'","")
+        log_result = log_result.replace("\\n ",'\n')[2:]
+        log_result = log_result.replace("\\n","\n")
+
+        f = open(self.TEST_DATA_FILE,"r")
+        input = f.read()
+        print(log_result == input)
 
         cf.cf_delete(self.APP_NAME)
         cf.cf_delete_service(self.DB_SERVICE_INSTANCE_NAME)
