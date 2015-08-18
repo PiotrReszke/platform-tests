a84ed21b730f162bb00839151ccfd183aecd4bfd
Konrad Reiter <konrad.reiter@intel.com>
2015-08-18 08:58:05 +0200

    create tests packages and improve unlock script

--- deploy/unlock.sh
+++ deploy/unlock.sh
@@ -1,7 +1,11 @@
-REPO_PATH=$(pwd)
-sudo apt-get install libssl-dev
-cd /tmp && wget https://github.com/AGWA/git-crypt/archive/0.5.0.zip
-unzip 0.5.0.zip && cd git-crypt-0.5.0/
-make && sudo make install
-echo $REPO_PATH
-cd $REPO_PATH && git-crypt unlock key.dat
+INSTALLED=$(which git-crypt)
+if [ -z $INSTALLED ]
+    then
+        REPO_PATH=$(pwd)
+        sudo apt-get install libssl-dev
+        cd /tmp && wget https://github.com/AGWA/git-crypt/archive/0.5.0.zip
+        unzip 0.5.0.zip && cd git-crypt-0.5.0/
+        make && sudo make install
+        cd $REPO_PATH
+fi
+git-crypt unlock key.dat
