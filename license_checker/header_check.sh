6b908f958e8b53410ae555af25dff4da574e3cc8
tciunel <tomasz.ciunel@intel.com>
2015-08-18 11:19:56 +0200

    DPNG-1661 Add copyright headers and tool to check proper formats

--- license_checker/header_check.sh
+++ license_checker/header_check.sh
@@ -14,21 +14,5 @@
 # limitations under the License.
 #
 
-import test_utils.api_calls.latest_events_service_api_calls as api
-from test_utils import get_admin_client
-
-
-class EventSummary(object):
-
-    def __init__(self, total, events):
-        self.total = total
-        self.events = events
-
-    @classmethod
-    def api_get_latest_events(cls, client=None):
-        client = client or get_admin_client()
-        response = api.api_get_latest_events(client)
-        return {
-            "total": response["total"],
-            "events": response["events"]
-        }
+mvn -f license_checker.xml license:check
+exit $?
