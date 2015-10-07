3fef1ae168dcff485b197eb0b3e47e0af3d688f4
Konrad Reiter <konrad.reiter@intel.com>
2015-10-07 16:51:30 +0200

    Hardcode version of requests library
    Due to changes in requests library it was necessary to hardcode version of requests that are inside requirements

--- deploy/create_virtualenv.sh
+++ deploy/create_virtualenv.sh
@@ -19,7 +19,7 @@
 REQUIREMENTS=`find . -name requirements.txt`
 PYVENV=~/virtualenvs/pyvenv_api_tests
 
-pyvenv-3.4 --without-pip $PYVENV &&
+python3 -m venv --without-pip $PYVENV &&
 source $PYVENV/bin/activate &&
 curl https://bootstrap.pypa.io/get-pip.py | python &&
 $PYVENV/bin/pip install -r $REQUIREMENTS &&
