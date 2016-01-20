7fe44adf343ee4e6940394bdf51ea841cb8c5cb0
ugolowic <urszula.golowicz@intel.com>
2016-01-20 22:13:16 +0100

    Add functional smoke tests with minimal prerequisites
    Simple functional tests: create, delete org, space, onboarding,
    adding user to org, space, create transfer, create instance, using
    seedorg, seedspace (or other reference org or space) and tester admin.

---
+++ project/tests/test_appstack/test_smoke.py
@@ -0,0 +1,152 @@
+#
+# Copyright (c) 2016 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+import unittest
+
+from test_utils import ApiTestCase, PlatformApiClient, CfApiClient, generate_csv_file
+from objects import Organization, User, Space, Transfer, DataSet, ServiceType, ServiceInstance
+
+
+class FunctionalSmokeTests(ApiTestCase):
+    TRANSFER_LINK = "http://fake-csv-server.gotapaas.eu/fake-csv/2"
+
+    @classmethod
+    def setUpClass(cls):
+        cls.step("Get reference organization and space")
+        cls.ref_org, cls.ref_space = Organization.get_ref_org_and_space()
+
+    def test_0_test_admin_can_login_to_platform(self):
+        PlatformApiClient.get_admin_client()
+
+    def test_create_and_delete_organization(self):
+        self.step("Create organization")
+        test_org = Organization.api_create()
+        self.step("Check that organization is on the list")
+        orgs = Organization.api_get_list()
+        self.assertInList(test_org, orgs)
+        self.step("Delete organization")
+        test_org.api_delete()
+        self.step("Check that the organization is not on the list")
+        orgs = Organization.api_get_list()
+        self.assertNotInList(test_org, orgs)
+
+    def test_onboarding(self):
+        self.step("Onboard new user")
+        test_user, test_org = User.api_onboard()
+        self.step("Check that user is created")
+        users = User.cf_api_get_all_users()
+        self.assertInList(test_user, users)
+        self.step("Check that organization is created")
+        orgs = Organization.api_get_list()
+        self.assertInList(test_org, orgs)
+
+    def test_add_new_user_to_and_delete_from_org(self):
+        self.step("Add new user to organization")
+        test_user = User.api_create_by_adding_to_organization(org_guid=self.ref_org.guid,
+                                                              roles=User.ORG_ROLES["auditor"])
+        self.step("Check that the user is added")
+        users = User.api_get_list_via_organization(org_guid=self.ref_org.guid)
+        self.assertInList(test_user, users)
+        self.step("Delete the user from the organization")
+        test_user.api_delete_from_organization(org_guid=self.ref_org.guid)
+        self.step("Check that the user is in the organization")
+        users = User.api_get_list_via_organization(org_guid=self.ref_org.guid)
+        self.assertNotInList(test_user, users)
+
+    def test_create_and_delete_space(self):
+        self.step("Create new space")
+        test_space = Space.api_create(org=self.ref_org)
+        self.step("Check that the space was created")
+        spaces = Space.api_get_list()
+        self.assertInList(test_space, spaces)
+        self.step("Delete the space")
+        test_space.api_delete()
+        self.step("Check that the space was deleted")
+        self.assertNotInListWithRetry(test_space, Space.api_get_list)
+
+    def test_add_new_user_to_and_delete_from_space(self):
+        self.step("Add new user to space")
+        test_user = User.api_create_by_adding_to_space(org_guid=self.ref_org.guid, space_guid=self.ref_space.guid)
+        self.step("Check that the user is added")
+        users = User.api_get_list_via_space(space_guid=self.ref_space.guid)
+        self.assertInList(test_user, users)
+        self.step("Remove the user from space")
+        test_user.api_delete_from_space(space_guid=self.ref_space.guid)
+        self.step("Check that the user was deleted")
+        users = User.api_get_list_via_space(space_guid=self.ref_space.guid)
+        self.assertNotInList(test_user, users)
+
+    def test_add_and_delete_transfer_from_link(self):
+        self.step("Create a transfer and check it has finished")
+        transfer = Transfer.api_create(category="other", source=self.TRANSFER_LINK, org=self.ref_org)
+        transfer.ensure_finished()
+        self.step("Check that the transfer is on the list")
+        transfers = Transfer.api_get_list(org_list=[self.ref_org])
+        self.assertInList(transfer, transfers)
+        self.step("Get data set matching to transfer")
+        data_set = DataSet.api_get_matching_to_transfer(org_list=[self.ref_org], transfer_title=transfer.title)
+        self.step("Delete the data set")
+        data_set.api_delete()
+        self.step("Check that the data set was deleted")
+        data_sets = DataSet.api_get_list(org_list=[self.ref_org])
+        self.assertNotInList(data_set, data_sets)
+        self.step("Delete the transfer")
+        transfer.api_delete()
+        self.step("Check that the transfer was deleted")
+        transfers = Transfer.api_get_list(org_list=[self.ref_org])
+        self.assertNotInList(transfer, transfers)
+
+    def test_add_and_delete_transfer_from_file(self):
+        self.step("Generate a test csv file")
+        file_path = generate_csv_file(column_count=10, row_count=100)
+        self.step("Create a transfer and check it has finished")
+        transfer = Transfer.api_create_by_file_upload(category="health", org=self.ref_org, file_path=file_path)
+        transfer.ensure_finished()
+        self.step("Check that the transfer is on the list")
+        transfers = Transfer.api_get_list(org_list=[self.ref_org])
+        self.assertInList(transfer, transfers)
+        self.step("Get data set for the transfer")
+        data_set = DataSet.api_get_matching_to_transfer(org_list=[self.ref_org], transfer_title=transfer.title)
+        self.step("Delete the data set")
+        data_set.api_delete()
+        self.step("Check that the data set was deleted")
+        data_sets = DataSet.api_get_list(org_list=[self.ref_org])
+        self.assertNotInList(data_set, data_sets)
+        self.step("Delete the transfer")
+        transfer.api_delete()
+        self.step("Check that the transfer was deleted")
+        transfers = Transfer.api_get_list(org_list=[self.ref_org])
+        self.assertNotInList(transfer, transfers)
+
+    def test_create_and_delete_marketplace_service_instances(self):
+        self.step("Get list of services in marketplace")
+        marketplace = ServiceType.api_get_list_from_marketplace(space_guid=self.ref_space.guid)
+        marketplace = [s for s in marketplace if s.label != "scoring-engine"]
+        for service in marketplace:
+            for plan in service.service_plans:
+                with self.subTest(service=service.label, plan=plan["name"]):
+                    self.step("Create instance")
+                    instance = ServiceInstance.api_create(org_guid=self.ref_org.guid, space_guid=self.ref_space.guid,
+                                                          service_label=service.label, service_plan_guid=plan["guid"])
+                    self.step("Check that the instance was created")
+                    instances = ServiceInstance.api_get_list(space_guid=self.ref_space.guid,
+                                                             service_type_guid=service.guid)
+                    self.assertInList(instance, instances)
+                    self.step("Delete the instance")
+                    instance.api_delete()
+                    self.step("Check that the instance was deleted")
+                    instances = ServiceInstance.api_get_list(space_guid=self.ref_space.guid,
+                                                             service_type_guid=service.guid)
+                    self.assertNotInList(instance, instances)
