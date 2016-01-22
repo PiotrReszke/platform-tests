3e0e38595d5e915184047056f123d46cc3726123
Bartlomiej <bartlomiejx.bedra@intel.com>
2016-01-22 11:23:44 +0100

    Add fix for scoring engine tests
    Model file url is provided in created transfer instead of downloading
    the file and uploading it to hdfs.

--- project/tests/test_api/services/test_scoring_engine.py
+++ project/tests/test_api/services/test_scoring_engine.py
@@ -13,24 +13,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-import unittest
-
-from test_utils import ApiTestCase, cleanup_after_failed_setup, AppClient, download_file, get_test_name
+from test_utils import ApiTestCase, cleanup_after_failed_setup, get_test_name
 from objects import Organization, ServiceInstance, ServiceInstanceKey, Transfer, DataSet
 
 
-@unittest.skip("Freezes on intel-data-kerberos-second")
 class TestScoringEngineInstance(ApiTestCase):
-    MODEL_FILE_NAME = "model.tar"
     MODEL_URL = "https://repo.gotapaas.eu/files/models_a8ab76353c2143509514da386d32a2f8.tar"
-    MODEL_FILE_TITLE = "test_model_file"
 
     @classmethod
-    def _create_transfer(cls, org, model_file, category="other"):
-        hdfs_client = AppClient.get_admin_client(client_type="app")
-        cls.step("Create a transfer")
-        transfer = Transfer.api_create_by_file_upload(category=category, org=org, file_path=model_file,
-                                                      title=cls.MODEL_FILE_TITLE, client=hdfs_client)
+    def _create_transfer(cls, org, model_url, category="other"):
+        transfer = Transfer.api_create(category=category, org=org, source=model_url)
         transfer.ensure_finished()
         return transfer
 
@@ -53,12 +45,10 @@ class TestScoringEngineInstance(ApiTestCase):
         cls.step("Create test organization and test spaces")
         cls.test_org = Organization.api_create(space_names=("test-space",))
         cls.test_space = cls.test_org.spaces[0]
-        cls.step("Download model file")
-        model_file_path = download_file(cls.MODEL_URL, cls.MODEL_FILE_NAME)
         cls.se_label = "scoring-engine"
         cls.se_plan = "Simple"
-        cls.step("Upload model file and get hdfs path")
-        t = cls._create_transfer(cls.test_org, model_file_path)
+        cls.step("Create a transfer and get hdfs path")
+        t = cls._create_transfer(cls.test_org, cls.MODEL_URL)
         ds = DataSet.api_get_matching_to_transfer([cls.test_org], t.title)
         cls.hdfs_path = ds.target_uri
 
