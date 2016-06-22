3cccefa656ca716ca22d438b38a7accf39054f7b
Adam Jakubowski <adam.jakubowski@codilime.com>
2016-06-22 16:04:11 +0200

    WIP - checking whether executor has started

--- project/tests/test_functional/data_science/test_seahorse.py
+++ project/tests/test_functional/data_science/test_seahorse.py
@@ -3,6 +3,10 @@ from modules.tap_object_model import ServiceInstance
 from configuration import config
 import requests
 import re
+import json
+import time
+from retry import retry
+import websocket
 
 class Seahorse(TapTestCase):
     seahorse_label = "k-seahorse-dev"
@@ -10,7 +14,51 @@ class Seahorse(TapTestCase):
     username = config.CONFIG["admin_username"]
     password = config.CONFIG["admin_password"]
 
-    def create_session(self, instance_url, domain, login, password):
+    def test_workflows_are_visible(self):
+        (session, seahorse_url) = self._create_session()
+        workflows = session.get(seahorse_url + '/v1/workflows').json()
+        assert len(workflows) > 5
+
+    def test_workflow_can_be_cloned(self):
+        (session, seahorse_url) = self._create_session()
+
+        workflows = session.get(seahorse_url + '/v1/workflows').json()
+
+        first_workflow = workflows[0]
+
+        clone_url = seahorse_url + '/v1/workflows/' + first_workflow["id"] + "/clone"
+        clone_resp = session.post(clone_url, json = {'name': 'Cloned workflow', 'description': 'Some desc'})
+        cloned_workflow_id = clone_resp.json()['workflowId']
+
+        session.get(seahorse_url + '/v1/workflows/' + cloned_workflow_id).json()
+        return cloned_workflow_id
+
+    def test_workflow_can_be_started(self):
+        (session, seahorse_url) = self._create_session()
+        
+        cloned_workflow_id = self.test_workflow_can_be_cloned()
+
+        session_url = seahorse_url + '/v1/sessions'
+        post_session_resp = session.post(session_url, json = {'workflowId': cloned_workflow_id})
+
+        self.ensure_executor_is_running(session, seahorse_url, cloned_workflow_id)
+
+        # ws = websocket.WebSocket()
+        # ws.connect("ws://example.com/websocket", http_proxy_host="proxy_host_name", http_proxy_port=3128)
+
+    @retry(AssertionError, tries=15, delay=5)
+    def ensure_executor_is_running(self, session, url, workflow_id):
+        session_url = url + '/v1/sessions'
+        sessions = list(
+            filter(lambda s: s['workflowId'] == workflow_id, session.get(session_url).json()['sessions']))
+        status = sessions[0]['status']
+        assert status == 'running'
+
+    def _create_session(self):
+        return self._create_session_local()
+
+    def _create_session_tap(self):
+        # TODO return session and url from TAP
         session = requests.Session()
         login_form = session.get(instance_url, verify=False)
 
@@ -23,21 +71,7 @@ class Seahorse(TapTestCase):
         print("CSRF {0}".format(csrf_value))
         payload = {'username': login, 'password': password, 'X-Uaa-Csrf': csrf_value}
         session.post(url='http://login.{0}/login.do'.format(domain), data=payload)
-        return session
+        return (session, url)
 
-    def test_create_instance_in_seahorse_org(self):
-        test_org_uuid = '3761da19-692c-4afb-95b6-f20d2d37ec3f'
-        test_space_uuid = 'b641e43e-b89b-4f32-b360-f1cf6bd1aa74'
-
-        service_instance = ServiceInstance.api_create_with_plan_name(test_org_uuid, test_space_uuid, self.seahorse_label,
-                                                      service_plan_name="free")
-
-        service_instance.ensure_created()
-        seahorse_url = "https://"+service_instance.name + service_instance.guid + "." + self.tap_domain
-        print(seahorse_url)
-        session=self.create_session(seahorse_url, domain=self.tap_domain, login=self.username, password=self.password)
-
-        resp=session.get(seahorse_url + '/v1/workflows')
-        print(resp.text)
-
-#{"ajk-test":{"guid":"c6078b05-491d-46a0-808f-8b500148b400","hostname":"ajk_test-c6078b05-491d-46a0-808f-8b500148b400.seahorse-krb.gotapaas.eu","login":"","password":""},"pr_20160622_120229_669492":{"guid":"9d05a0d5-8951-4283-9d7e-21b966fcc39c","hostname":"pr_20160622_120229_669492-9d05a0d5-8951-4283-9d7e-21b966fcc39c.seahorse-krb.gotapaas.eu","login":"","password":""},"test":{"guid":"c1d26a50-d20a-40d6-aba4-342123b1e75a","hostname":"test-c1d26a50-d20a-40d6-aba4-342123b1e75a.seahorse-krb.gotapaas.eu","login":"","password":""},"xyz":{"guid":"8eff882a-649d-4333-bb27-bbb20ff6b80b","hostname":"xyz-8eff882a-649d-4333-bb27-bbb20ff6b80b.seahorse-krb.gotapaas.eu","login":"","password":""},"piotr_test":{"guid":"c81c884c-2a29-44c6-b0ee-809fd5c61ad8","hostname":"piotr_test-c81c884c-2a29-44c6-b0ee-809fd5c61ad8.seahorse-krb.gotapaas.eu","login":"","password":""},"pr_20160622_114329_754114":{"guid":"681e29e5-2e78-4b9a-bd98-683710a1ff93","hostname":"pr_20160622_114329_754114-681e29e5-2e78-4b9a-bd98-683710a1ff93.seahorse-krb.gotapaas.eu","login":"","password":""},"pr_20160622_114142_861305":{"guid":"cef91405-32b0-4dd3-81ba-49224c538716","hostname":"pr_20160622_114142_861305-cef91405-32b0-4dd3-81ba-49224c538716.seahorse-krb.gotapaas.eu","login":"","password":""}}
+    def _create_session_local(self):
+        return (requests.Session(), "http://localhost:8080")
